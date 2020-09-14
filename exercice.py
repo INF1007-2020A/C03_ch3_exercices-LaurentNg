ae8604cc8753e483c164df4b1228ee9c0e9356b5
LaurentNg <71021604+LaurentNg@users.noreply.github.com>
2020-09-14 15:04:30 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -11,7 +11,7 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    radian = angle_degs + angle_mins/60 + angle_secs/3600
+    radian = (angle_degs + angle_mins/60 + angle_secs/3600)*math.pi/180
     return radian
    
 
