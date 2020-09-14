8996bde50c83b9c557e3742232029d6f9ddfec5c
LaurentNg <71021604+LaurentNg@users.noreply.github.com>
2020-09-14 14:36:15 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -11,9 +11,9 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    
-    return 0.0
-
+    radian = angle_degs + angle_mins/60 + angle_secs/3600
+    return radian
+   
 
 def to_degrees(angle_rads: float) -> tuple:
     return 0.0, 0.0, 0.0
